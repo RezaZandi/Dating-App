e219520a42394efec6c3f77a743ca6ca8cf3724a
Reza <Rezazandirz@gmail.com>
2020-06-26 13:26:04 -0400

    attempting to make heroku work

--- dating_project/settings.py
+++ dating_project/settings.py
@@ -138,11 +138,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-
-STATICFILES_DIR = [
-"/Dating-App/dating_app/static",
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 
