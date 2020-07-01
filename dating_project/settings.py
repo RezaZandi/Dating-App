207b83a293c7ddd951af8e6dde318fe5bd2e7704
Reza <Rezazandirz@gmail.com>
2020-07-01 15:32:46 -0400

    added whitenoise correct version

--- dating_project/settings.py
+++ dating_project/settings.py
@@ -145,7 +145,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'dating_app/media')
