6bfa99929142ead4f89ca1725fddddb033844a03
Reza <Rezazandirz@gmail.com>
2020-07-02 12:24:57 -0400

    changed media root

--- dating_project/settings.py
+++ dating_project/settings.py
@@ -166,7 +166,7 @@ STATICFILES_DIRS = (
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'dating_app/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 #Settings for django-bootstrap3
