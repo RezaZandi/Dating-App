a3c5a6e54c681e777b2e6de077b533ecbbabc32b
Reza <Rezazandirz@gmail.com>
2020-07-02 18:40:42 -0400

    heroku

--- dating_project/settings.py
+++ dating_project/settings.py
@@ -187,7 +187,7 @@ AWS_STORAGE_BUCKET_NAME = 'cupids-corner'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-#STATIC_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 '''
