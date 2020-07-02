4c31f85dc80e5f2f3b7f09a9c00bf7fbeb327677
Reza <Rezazandirz@gmail.com>
2020-07-01 20:10:20 -0400

    heroku

--- dating_project/settings.py
+++ dating_project/settings.py
@@ -163,7 +163,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'dating_app/media')
