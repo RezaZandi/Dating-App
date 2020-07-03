f4b6f4e2e78f9a1914c67c61d047e6690b205953
Reza <Rezazandirz@gmail.com>
2020-07-03 18:02:17 -0400

    testing that AWS is working

--- dating_project/settings.py
+++ dating_project/settings.py
@@ -190,7 +190,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
-'''
+''''
 <?xml version="1.0" encoding="UTF-8"?>
 <CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
 <CORSRule>
