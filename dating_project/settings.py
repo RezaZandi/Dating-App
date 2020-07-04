4ebf0b1adcd5953dfec05e11ebf5594d4dcd3864
Reza <Rezazandirz@gmail.com>
2020-07-03 22:55:38 -0400

    heroku

--- dating_project/settings.py
+++ dating_project/settings.py
@@ -120,7 +120,7 @@ DATABASES = {
 }
 
 
-# Password validation
+## Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
