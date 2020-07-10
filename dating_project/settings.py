58224f41a231d9d551cd9c39a995170cd8a938aa
Reza <Rezazandirz@gmail.com>
2020-07-10 17:21:29 -0400

    testing

--- dating_project/settings.py
+++ dating_project/settings.py
@@ -127,7 +127,7 @@ db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
 #For local postgres
-'''
+'''#
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
