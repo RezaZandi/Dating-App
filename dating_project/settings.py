5fa4a53b4a4de322136dbe89f78bd325c1b94acf
Reza <Rezazandirz@gmail.com>
2020-07-10 17:29:24 -0400

    using aws rds

--- dating_project/settings.py
+++ dating_project/settings.py
@@ -122,10 +122,11 @@ DATABASES = {
         'PORT' : '5432'
     }
 }
+'''
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
-
+'''
 #For local postgres
 '''#
 DATABASES = {
@@ -140,13 +141,14 @@ DATABASES = {
 
 }
 '''
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
 ## Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
