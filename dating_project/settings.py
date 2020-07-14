e23fe94a2ef356da19cacec59d6b34c4c5795b79
Reza <Rezazandirz@gmail.com>
2020-07-14 18:32:47 -0400

    debug false

--- dating_project/settings.py
+++ dating_project/settings.py
@@ -19,12 +19,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '(mui%^85te&u)5&@zwo3b^u=em$vgn1fb5k69!o_j$f5gg6d&o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ADMINS = (('Reza', 'Rezazandirz@gmail.com'),)
 
